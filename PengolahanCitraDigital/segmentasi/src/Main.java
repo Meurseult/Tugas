f1ce60c1c4aec41ebc8843fc506f80f0b51e782e
anggraanutomo <anggraalheranasmitautomo@gmail.com>
2022-12-24 14:39:10 +0700

    add question and output program

--- PengolahanCitraDigital/segmentasi/src/Main.java
+++ PengolahanCitraDigital/segmentasi/src/Main.java
@@ -1,19 +1,62 @@
 /*
+    author : anggra alhera nasmita utomo
+    email : anggraalheranasmitautomo@gmail.com
 
+    Soal : Diketahui citra berukuran 5 x 5 dengan nilai piksel sebagai berikut:
+    80 80 100 200 180
+    200 10 30 25 130
+    250 251 250 200 80
+    255 255 110 100 120
+    255 100 180 200 80
+
+    1. Gambarkan citra hasil Manual Thresholding dengan nilai threshold 80
+    2. Gambarkan citra hasil Iterative Thresholding dengan nilai T0 = 100
+    dan T awal = 130. lakukan proses iteratif sampai dengan iteratif ke 5
+    jika proses iteratif belum selesai
+
+    output program :
+
+        Hasil Segmentasi Citra dengan Threshold Manual 80
+        1 1 1 1 1
+        1 0 0 0 1
+        1 1 1 1 1
+        1 1 1 1 1
+        1 1 1 1 1
+
+        Hasil Segmentasi Citra dengan Threshold Iteratif T0 = 100 T_AWAL = 130
+        Iterasi ke-1
+        T1 = [80, 80, 100, 10, 30, 25, 130, 80, 110, 100, 120, 100, 80]
+        T2 = [200, 180, 200, 250, 251, 250, 200, 255, 255, 255, 180, 200]
+        Jumlah T1: 1045
+        Jumlah T2: 2676
+        Rata-rata T1: 80
+        Rata-rata T2: 223
+        Rata-rata Threshold: 151
+
+
+        Iterasi ke-2
+        T1 = [80, 80, 100, 10, 30, 25, 130, 80, 110, 100, 120, 100, 80]
+        T2 = [200, 180, 200, 250, 251, 250, 200, 255, 255, 255, 180, 200]
+        Jumlah T1: 1045
+        Jumlah T2: 2676
+        Rata-rata T1: 80
+        Rata-rata T2: 223
+        Rata-rata Threshold: 151
+
+        Process finished with exit code 0
  */
-
 
 
 public class Main
 {
     public static void main(String[] args)
     {
-        final int CITRA [][] = {
-                {80,80,100,200,180},
-                {200,10,30,25,130},
-                {250,251,250,200,80},
-                {255,255,110,100,120},
-                {255,100,180,200,80}
+        final int CITRA[][] = {
+                {80, 80, 100, 200, 180},
+                {200, 10, 30, 25, 130},
+                {250, 251, 250, 200, 80},
+                {255, 255, 110, 100, 120},
+                {255, 100, 180, 200, 80}
         };
 
         final int THRESHOLD_MANUAL_VALUE = 80;
